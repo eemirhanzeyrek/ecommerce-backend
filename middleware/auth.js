11846e6820fd033db424fb285c1a85ac33560636
eemirhanzeyrek <contactemirhanzeyrek@gmail.com>
2025-03-08 18:41:56 +0300

    auth middleware operations performed

---
+++ middleware/auth.js
@@ -0,0 +1,37 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const authMidd = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(500).json({
+      message: "log in to access",
+    });
+  }
+
+  const decodedData = jwt.verify(token, "USER_SECRET_TOKEN");
+
+  if (!decodedData) {
+    return res.status(500).json({
+      message: "your access token is invalid",
+    });
+  }
+
+  req.user = await User.findById(decodedData.id);
+
+  next();
+};
+
+const roleChecked = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(500).json({
+        message: "you do not have permission to log in",
+      });
+    }
+    next();
+  };
+};
+
+module.exports = { authMidd, roleChecked };
--- routes/product.js
+++ routes/product.js
@@ -8,15 +8,16 @@ const {
   createReview,
   adminProducts,
 } = require("../controllers/product");
+const { authMidd, roleChecked } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/products", allProducts);
-router.get("/admin/products", adminProducts);
+router.get("/admin/products", authMidd, roleChecked("admin"), adminProducts);
 router.get("/products/:id", detailProducts);
-router.post("/product/new", createProduct);
-router.post("/product/newReview", createReview);
-router.delete("/products/:id", deleteProduct);
-router.put("/products/:id", updateProduct);
+router.post("/product/new", authMidd, roleChecked("admin"), createProduct);
+router.post("/product/newReview", authMidd, createReview);
+router.delete("/products/:id", authMidd, roleChecked("admin"), deleteProduct);
+router.put("/products/:id", authMidd, roleChecked("admin"), updateProduct);
 
 module.exports = router;
